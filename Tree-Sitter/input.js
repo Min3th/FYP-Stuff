f0b6826768a762196fb3ea58d3b152f6a8f7a9f2
Min3th <minethweerasinghe@gmail.com>
2025-07-18 14:11:39 +0530

    minor file change

--- Tree-Sitter/input.js
+++ Tree-Sitter/input.js
@@ -1,1 +1,8 @@
-input.js;
+//@service UserService
+//@route /register
+//@auth protected
+function registerUser(req, res) {
+  const { username, password } = req.body;
+  // logic to register user
+  res.send({ status: "User registered" });
+}
--- input.js
+++
@@ -1,8 +0,0 @@
-//@service UserService
-//@route /register
-//@auth protected
-function registerUser(req, res) {
-  const { username, password } = req.body;
-  // logic to register user
-  res.send({ status: "User registered" });
-}
